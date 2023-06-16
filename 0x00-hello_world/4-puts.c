f1f7bdb070a5dd55f707c8a407f57ccb20a6aa4a
zainnabe <bnfzineb94@gmail.com>
2023-06-16 14:50:38 -0700

    Upload puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line
+ * return:0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
