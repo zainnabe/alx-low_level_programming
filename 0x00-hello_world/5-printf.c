1aae7d77dc2c5921b51adb1a770678d54e00c60c
zainnabe <bnfzineb94@gmail.com>
2023-06-16 15:20:22 -0700

    Upload size

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-#!/bin/bash
+#include <stdio.h>
 /**
- * main - A program that prints a line using printf function
- * Return :0 (Success)
+ *  main - A program that prints a line using printf function
+ *  Return: 0 (Success)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
