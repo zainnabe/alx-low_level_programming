2eedb18a01fa9aab030b9feb982da66b4d4b0e0a
zainnabe <bnfzineb94@gmail.com>
2023-06-18 18:55:39 -0700

    Update tebahpla

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - A program that prints ll the numbers of base 16 in lowercase
+* Return: 0 (Success)
+*/
+
+int main(void)
+{
+char c;
+int d;
+c = 'a';
+d = 0;
+while (d <10)
+{
+putchar(d + '0');
+c++;
+}
+while (c <= 'f')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
