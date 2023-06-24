43f533e8c3ad73ad40b3fa964dc20d10ff192b74
zainnabe <bnfzineb94@gmail.com>
2023-06-23 18:37:44 -0700

    Update is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* is_upper - Check if a letter is upper
+* @x: The number to be checked
+* Return: 1 for upper letter or 0 for any else
+*/
+
+int is_upper(int x)
+{
+if (x >= 65 && x <= 90)
+{
+return (1);
+}
+return (0);
+}
+
