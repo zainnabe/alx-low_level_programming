49d19358e26eaf04d36e0b3128b21d0aad1ed571
zainnabe <bnfzineb94@gmail.com>
2023-06-26 18:28:27 -0700

    rev

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,17 +3,15 @@
 /**
 * _strlen - Returns the length of a string
 * @s: string
-* @l: integer
 * Return: length
 */
 int _strlen(char *s)
 {
-int l;
-l = 0;
+int longi = 0;
 while (*s != '\0')
 {
-l++;
+longi++;
 s++;
 }
-return (l);
+return (longi);
 }
